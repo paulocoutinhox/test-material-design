07a19837dd47293e5ec11718130a29859094a298
Paulo Coutinho <paulo@prsolucoes.com>
2015-09-04 18:37:01 -0300

    initial commit

---
+++ app/src/main/java/br/com/ubook/testmaterialdesign/MainActivity.java
@@ -0,0 +1,58 @@
+package br.com.ubook.testmaterialdesign;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.WindowManager;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    public static Toolbar toolbar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
+        loadToolbar("Meu aplicativo");
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    protected Toolbar loadToolbar(String title) {
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if(toolbar != null){
+            if(!TextUtils.isEmpty(title)){
+                toolbar.setTitle(title);
+            }
+            setSupportActionBar(toolbar);
+        }
+        return null;
+    }
+
+}
